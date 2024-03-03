bbcc5dfc54986ac778595ee7bd9c7e875ccb62a2
Mohamed Belaouer <mohamedbelaouer93@gmail.com>
2024-03-03 17:50:23 +0100

    project init

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        // 👇 Add CSS variables
+        poppins: ["var(--font-poppins)"],
+        clapat: ["var(--font-clapat)"],
+      },
     },
   },
   plugins: [],
